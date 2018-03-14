37d53a217811f8b0bef45d22bee8fd31119c6064
shamwairock <shamwairock@gmail.com>
2018-03-14 14:49:13 +0800

    [14/3/2018]Var vs Dynamic Variable

---
+++ Var vs Dynamic Variable/Var vs Dynamic Variable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Var_vs_Dynamic_Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj1 = "obj1 HelloWorld";
+            dynamic obj2;
+
+            Console.WriteLine(obj1);
+
+            obj2 = "obj2 HelloWorld2";
+            Console.WriteLine(obj2);
+
+            obj2 = 1;
+            Console.WriteLine(obj2);
+
+            Console.ReadKey();
+        }
+    }
+}
