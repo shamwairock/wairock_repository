ef118baf396cc82ea2b25732eb7f288ed4108022
shamwairock <shamwairock@gmail.com>
2018-03-14 17:01:41 +0800

    [14/3/2018]Yield vs Return Statement

---
+++ Yield vs Return Statement/Yield vs Return Statement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Yield_vs_Return_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SimpleReturn());
+            Console.WriteLine(SimpleReturn());
+            Console.WriteLine(SimpleReturn());
+
+            YieldReturn().ToList().ForEach(Console.WriteLine);
+
+            Console.ReadKey();
+        }
+
+        static int SimpleReturn()
+        {
+            return 1;
+            return 2;
+            return 3;
+        }
+
+        static IEnumerable<int> YieldReturn()
+        {
+            Console.WriteLine("Step 1");
+            yield return 1;
+
+            Console.WriteLine("Step 2");
+            yield return 2;
+
+            Console.WriteLine("Step 3");
+            yield return 3;
+
+            Console.WriteLine("Step 4");
+        }
+    }
+}
