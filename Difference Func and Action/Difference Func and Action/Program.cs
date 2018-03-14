5b70c99057997d76ee2ba2d681e73c12f82465e0
shamwairock <shamwairock@gmail.com>
2018-03-14 15:11:54 +0800

    [14/3/2018]Func vs Action

---
+++ Difference Func and Action/Difference Func and Action/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Difference_Func_and_Action
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> myAction = DoSomething;
+            myAction(123);           // Prints out "123"
+                                     // can be also called as myAction.Invoke(123);
+
+            Func<int, int, double> myFunc = new Func<int, int, double>(CalculateSomething);
+            Console.WriteLine(myFunc(5,2));   // Prints out "2.5"
+
+            Console.ReadKey();
+        }
+
+        static void DoSomething(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+        static double CalculateSomething(int i, int j)
+        {
+            try
+            {
+                return (double) i/j;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+        }
+    }
+}
