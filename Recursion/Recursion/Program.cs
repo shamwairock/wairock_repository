1f77dba618b2760f48dbccb92d68aae770b998ee
shamwairock <shamwairock@gmail.com>
2018-03-16 17:22:21 +0800

    [16/3/2018]Recursion

--- Recursion/Recursion/Program.cs
+++ Recursion/Recursion/Program.cs
@@ -9,15 +9,29 @@ namespace Recursion
     {
         static void Main(string[] args)
         {
-            Factorial factorial = new Factorial();
-            int result = 0;
-            factorial.GetFactorial(5, ref result);
-            Console.WriteLine(result);
-            Console.WriteLine(factorial.GetFactorial(5));
+            Console.WriteLine(Recursion.GetSumOfDigit(999));
+            Console.WriteLine(Recursion.GetFactorialNumber(5));
+            Console.WriteLine(Recursion.ConvertIntToBinaryFormat(599));
+            Console.WriteLine(Recursion.PowerOfNumber(2,5));
 
+            var linkedList = new LinkedList<string>();
+            linkedList.AddFirst("you");
+            linkedList.AddLast("change");
+            linkedList.AddLast("nothing");
+            linkedList.AddLast("1");
+            linkedList.AddLast("2");
+            linkedList.AddLast("3");
 
-            SumDigit sumDigit = new SumDigit();
-            Console.WriteLine(sumDigit.GetSumOfDigit(999));
+            foreach (var s in linkedList)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+           
+            foreach (var s in Recursion.ReverseLinkedList(linkedList))
+            {
+                Console.Write(s + " ");
+            }
 
             Console.ReadKey();
         }
---
+++ Recursion/Recursion/Recursion.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recursion
+{
+    public static class Recursion
+    {
+        public static uint GetFactorialNumber(uint input)
+        {
+            if (input == 1)
+            {
+                return 1;
+            }
+
+            return GetFactorialNumber(input - 1) * input;
+        }
+
+        public static uint GetSumOfDigit(uint input)
+        {
+            if (input < 10)
+            {
+                return input;
+            }
+            
+            
+            var leftDigit = Convert.ToUInt32(input.ToString().First().ToString());
+            var residue = Convert.ToUInt32(new string(input.ToString().Skip(1).ToArray()));
+
+            return leftDigit + GetSumOfDigit(residue);
+        }
+
+        public static string ConvertIntToBinaryFormat(uint input)
+        {
+            var result = ConvertIntToBinary(input);
+           return new string(result.ToCharArray().Reverse().ToArray());
+        }
+
+        private static string ConvertIntToBinary(uint input)
+        {
+            if (input == 1)
+            {
+                return input.ToString();
+            }
+
+            var dividend = input;
+            var divisor = 2;
+
+            var quotient = (uint)(dividend/divisor);
+            var remainder = dividend%divisor;
+
+            return remainder + ConvertIntToBinary(quotient);
+        }
+
+        public static int PowerOfNumber(int baseNumber, int power)
+        {
+            if (power == 1)
+            {
+                return baseNumber;
+            }
+
+            return baseNumber* PowerOfNumber(baseNumber, power - 1);
+        }
+
+        public static LinkedList<string> ReverseLinkedList(LinkedList<string> input)
+        {
+            if (input.Count == 0)
+            {
+                return input;
+            }
+
+            var firstValue = input.First.Value;
+            input.RemoveFirst();
+            ReverseLinkedList(input).AddLast(firstValue);
+
+            return input;
+        }
+    }
+}
