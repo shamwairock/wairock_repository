c31efb9a38565039f9bdbdd902adaeb986f89a44
shamwairock <shamwairock@gmail.com>
2018-03-16 09:58:24 +0800

    [15/3/2018]Recursion

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial factorial = new Factorial();
+            int result = 0;
+            factorial.GetFactorial(5, ref result);
+            Console.WriteLine(result);
+            Console.WriteLine(factorial.GetFactorial(5));
+
+
+            SumDigit sumDigit = new SumDigit();
+            Console.WriteLine(sumDigit.GetSumOfDigit(999));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Recursion/Recursion/SumDigit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Recursion
+{
+    public class SumDigit
+    {
+        public int GetSumOfDigit(int input)
+        {
+            if (input < 10)
+            {
+                return input;
+            }
+
+            var left = GetMostLeftDigit(input);
+            var residue = GetResidueDigit(input);
+
+            return GetSumOfDigit(residue) + left;
+        }
+
+        private int GetMostLeftDigit(int input)
+        {
+            return input.ToString().First();
+        }
+
+        private int GetResidueDigit(int input)
+        {
+            var residue = input.ToString().Skip(1).ToArray();
+            string s = new string(residue);
+            return int.Parse(s);
+        }
+    }
+}
