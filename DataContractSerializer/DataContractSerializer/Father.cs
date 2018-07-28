74e605fe506ba5a21fef40f3afe648dc4335f423
shamwairock <shamwairock@gmail.com>
2018-07-28 15:48:20 +0800

    Commit DataContractSerializer and WpfDataValidation

---
+++ DataContractSerializer/DataContractSerializer/Father.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataContractSerializerDemo
+{
+   
+    [DataContract]
+    public class Father : GrandFather
+    {
+        [DataMember]
+        public string Age { get; set; }
+    }
+}
---
+++ DataContractSerializer/DataContractSerializer/GrandFather.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DataContractSerializerDemo
+{
+    [DataContract]
+    [KnownType(typeof(Father))]
+    public abstract class GrandFather
+    {
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
