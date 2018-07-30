745f2c42635c8a1cac42031ba9a687de25ddda57
shamwairock <shamwairock@gmail.com>
2018-07-30 18:28:43 +0800

    Binary Tree 1

---
+++ BinaryTree/BinaryTree/INode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public interface INode<T>
+    {
+        T Data {get;set;}
+
+        INode<T> LeftNode { get; set; }
+
+        INode<T> RightNode { get; set; }
+        
+        /// <summary>
+        /// Insertion is always insert to the left if the input is smaller than the root.
+        /// If node is duplicated, do not insert.
+        /// </summary>
+        /// <param name="node"></param>
+        void Insert(INode<T> node);
+
+        void Remove(INode<T> node);
+
+        void Traversal();
+
+        void PrintTree();
+
+        INode<T> BreathFirstSearch(INode<T> node);
+
+        INode<T> DepthFirstSearch(INode<T> node);
+
+        /// <summary>
+        /// Returns the data in the furthest left node
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        T GetMin(INode<T> node);
+
+        /// <summary>
+        /// Return the data in the furthest right node
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        T GetMax(INode<T> node);
+    }
+}
