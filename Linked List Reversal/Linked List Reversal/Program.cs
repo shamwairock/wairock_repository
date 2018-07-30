b4e957cea55aba6edbeb017d0746746575aa9c27
shamwairock <shamwairock@gmail.com>
2018-07-30 19:43:50 +0800

    linked list

---
+++ Linked List Reversal/Linked List Reversal/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linked_List_Reversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> myLinkedList = new LinkedList<string>();
+            myLinkedList.AddLast("1");
+            myLinkedList.AddLast("2");
+            myLinkedList.AddLast("3");
+            myLinkedList.AddLast("4");
+            myLinkedList.AddLast("5");
+            myLinkedList.AddLast("6");
+            myLinkedList.AddLast("7");
+            myLinkedList.AddLast("8");
+            myLinkedList.AddLast("9");
+            myLinkedList.AddLast("10");
+            myLinkedList.AddLast("11");
+            myLinkedList.AddLast("12");
+
+
+            foreach (var node in myLinkedList)
+            {
+                Console.Write(node + " ");
+            }
+
+            Console.WriteLine();
+
+            var head = myLinkedList.First;
+            var tail = myLinkedList.Last;
+
+            //Reverse
+            for (int i = 0; i < myLinkedList.Count - 1 ; i++)
+            {
+                if (i == 0)
+                {
+                    head.List.AddFirst(head.List.Last.Value);
+                    head = myLinkedList.First;
+                }
+                else
+                {
+                    myLinkedList.AddAfter(head, head.List.Last.Value);
+                    head = head.Next;
+                }
+
+                myLinkedList.RemoveLast();
+
+            }
+
+
+            foreach (var node in myLinkedList)
+            {
+                Console.Write(node + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Linked List SortedMerge/Linked List SortedMerge/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Linked_List_SortedMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list1 = new LinkedList<int>();
+            list1.AddLast(1);
+            list1.AddLast(2);
+            list1.AddLast(3);
+            list1.AddLast(4);
+            list1.AddLast(5);
+            list1.AddLast(9999);
+            list1.AddLast(100000);
+            list1.AddLast(22323232);
+
+            var list2 = new LinkedList<int>();
+            list2.AddLast(2);
+            list2.AddLast(4);
+            list2.AddLast(8);
+
+            var ihead = list1.First;
+            var jhead = list2.First;
+
+            while (list2.Count != 0)
+            {
+                for(int i=0;i< list1.Count; i++)
+                {
+                    if (jhead.Value <= ihead.Value)
+                    {
+                        ihead.List.AddBefore(ihead, jhead.Value);
+                        break;
+                    }
+                    else
+                    {
+                        ihead = ihead.Next;
+                        continue;
+                    }
+                }
+
+                if (list2.Count == 0)
+                {
+                    break;
+                }
+
+                list2.RemoveFirst();
+                jhead = list2.First;
+            }
+
+            foreach (var node in list1)
+            {
+                Console.Write(node + " ");
+            }
+
+            Console.Read();
+
+        }
+    }
+}
