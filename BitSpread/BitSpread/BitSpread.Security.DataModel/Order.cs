aaafc14ba322eb31fa4855cf6a9264e1e62e5105
shamwairock <shamwairock@gmail.com>
2019-08-28 22:49:53 +0800

    Implement WebApi V1

--- BitSpread/BitSpread/BitSpread.Security.DataModel/Order.cs
+++ BitSpread/BitSpread/BitSpread.Security.DataModel/Order.cs
@@ -4,10 +4,11 @@ namespace BitSpread.Security.DataModel
 {
     public class Order
     {
+        public string OrderId { get; set; }
         public DateTime Timestamp { get; set; }
         public double Volume { get; set; }
-    
         public double Price { get; set; }
         public OrderType OrderType { get; set; }
+        public string SecurityCode { get; set; }
     }
 }
